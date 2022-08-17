631ddf4f328dc56c7abfcc975e0bcc7beda7e151
Lonardo17 <98488112+Lonardo17@users.noreply.github.com>
2022-08-17 12:41:12 +0300

    home-work1

---
+++ node-hw1/contacts.js
@@ -0,0 +1,68 @@
+const fs = require ('fs');
+const FsPromises = fs.promises
+const path = require('path');
+
+  const contactsPath = path.resolve('./db/contacts.json');
+
+  async function readListContacts(){
+    const list = await FsPromises.readFile(contactsPath,"utf-8")
+    return JSON.parse(list);
+  }
+
+  async function changeListContacts(list){
+    return await FsPromises.writeFile(contactsPath,JSON.stringify(list),"utf-8")
+  }
+
+async function listContacts() {
+    try{
+        const list = await readListContacts();
+        console.log(list)
+    }
+    catch{
+        console.log(error.message);
+    }
+  }
+  
+  async function getContactById(contactId) {
+    try{
+        const list = await readListContacts();
+        const result = list.find(r => r.id == contactId)
+        console.log(result)
+    }
+    catch{
+        console.log(error.message);
+    }
+  }
+  
+  async function removeContact(contactId) {
+    try{
+        const list = await readListContacts();
+        const i = list.findIndex(r => r.id == contactId);
+        const remove = list.splice(i,1);
+        await changeListContacts(list); 
+        console.log(list)
+  }
+  catch{
+    console.log(error.message);
+}
+}
+  
+  async function addContact(name, email, phone) {
+    try{
+        const list = await readListContacts();
+        const id = list.reduce((acc,num)=> acc>Number(num.id)?acc:acc=Number(num.id),0)
+        const newArray = {id: `${id+1}`,name,email,phone};
+       await changeListContacts([...list, newArray]); 
+       const result = await readListContacts();
+        console.log(result)
+    }
+    catch{
+        console.log(error);
+    }
+  }
+  module.exports = {
+    listContacts,
+    getContactById,
+    removeContact,
+    addContact
+  }
